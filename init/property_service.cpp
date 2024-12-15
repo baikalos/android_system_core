c05e47fbd13cd2de6325b0dff0f612c7612c7d01
Sergei Vasilev <svasiliev22@yandex.ru>
2024-12-15 12:11:14 +0300

    Revert forgotten debug changes
    Change-Id: I5765b9ba06cfb7ab9c1def35b739b2243942cfc4

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -772,7 +772,7 @@ static void LoadPropertiesFromSecondStageRes(std::map<std::string, std::string>*
 // So we need to apply the same rule of build/make/tools/post_process_props.py
 // on runtime.
 static void update_sys_usb_config() {
-    bool is_secure = false; // android::base::GetBoolProperty("ro.adb.secure", /*true*/ false);
+    bool is_secure = android::base::GetBoolProperty("ro.adb.secure", true);
     std::string config = android::base::GetProperty("persist.sys.usb.config", "");
     // b/150130503, add (config == "none") condition here to prevent appending
     // ",adb" if "none" is explicitly defined in default prop.
